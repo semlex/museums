15e86ff8c5f2bf5d382e4ed6563e8838f1f1f4cd
semlex <picasso.semenov.spb@gmail.com>
2024-05-09 16:27:01 +0300

    change viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="ru">
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
         <meta name="emotion-insertion-point" content="" />
         <link rel="icon" href="/museum.png" />
         <DocumentHeadTags {...props} />
